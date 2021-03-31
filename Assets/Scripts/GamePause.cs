b2e15d7c3e6ce65bedcc5b1f99840eb0f028116e
Sleentex <56062311+Sleentex@users.noreply.github.com>
2021-03-31 14:19:47 +0300

    Fix

--- Assets/Scripts/GamePause.cs
+++ Assets/Scripts/GamePause.cs
@@ -8,6 +8,7 @@ public class GamePause : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject panelExit;
 
     // Update is called once per frame
     void Update()
@@ -28,6 +29,7 @@ public class GamePause : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        panelExit.SetActive(false);
         Time.timeScale = 1F;
         GameIsPaused = false; 
     }
@@ -36,6 +38,7 @@ public class GamePause : MonoBehaviour
     private void Pause()
     {
         pauseMenuUI.SetActive(true);
+        panelExit.SetActive(false);
         Time.timeScale = 0F; // екран зупиняється, від 0 до 1, 1 це норм хід ігри
         GameIsPaused = true;
     }
@@ -44,6 +47,18 @@ public class GamePause : MonoBehaviour
     {
         Time.timeScale = 1F;
         SceneManager.LoadScene("Menu");
+    }
+
+    public void ShowExitButtons()
+    {
+        pauseMenuUI.SetActive(false);
+        panelExit.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        panelExit.SetActive(false);
     }
 
     public void QuitGame()
